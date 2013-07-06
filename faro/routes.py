28e79271b87ef411e09196445c770cd1c9aa77fa
denneh <dj1042@gmail.com>
2013-07-06 13:58:33 -0500

    Added dashboard template

--- faro/routes.py
+++ faro/routes.py
@@ -24,6 +24,18 @@ def home():
 def about():
 	return render_template('about.html')
 	
+@app.route('/dash')
+def dash():
+	if 'email' not in session:
+		return redirect(url_for('signin'))
+		
+	user = User.query.filter_by(email = session['email']).first()
+	
+	if user is None:
+		return redirect(url_for('signin'))
+	else:
+		return render_template('dash.html', user=user)
+	
 @app.route('/contact', methods=['GET', 'POST'])
 def contact():
 	form = ContactForm()
@@ -37,31 +49,6 @@ def contact():
 		
 	elif request.method == 'GET':
 		return render_template('contact.html', form=form)
-
-@app.route('/testdb')
-def testdb():
-	if db.session.query("1").from_statement("SELECT 1").all():
-		return render_template('database.html', works=True)
-	else:
-		return render_template('database.html', works=False)
-		
-@app.route('/signup', methods=['GET', 'POST'])
-def signup():
-	form = SignupForm()
-	
-	if request.method == 'POST':
-		if form.validate() == False:
-			return render_template('signup.html', form=form)
-		else:
-			newuser = User(form.firstname.data, form.lastname.data, form.email.data, form.password.data)
-			db.session.add(newuser)
-			db.session.commit()
-
-			session['email'] = newuser.email			
-			return redirect(url_for('profile'))		
-			
-	elif request.method == 'GET':
-		return render_template('signup.html', form=form)
 		
 @app.route('/profile')
 def profile():
@@ -85,10 +72,28 @@ def signin():
 			return render_template('home.html', form=form)
 		else:
 			session['email'] = form.email.data
-			return redirect(url_for('profile'))
+			return redirect(url_for('dash'))
 			
 	elif request.method == 'GET':
 		return render_template('home.html', form=form)
+		
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+	form = SignupForm()
+	
+	if request.method == 'POST':
+		if form.validate() == False:
+			return render_template('signup.html', form=form)
+		else:
+			newuser = User(form.firstname.data, form.lastname.data, form.email.data, form.password.data)
+			db.session.add(newuser)
+			db.session.commit()
+
+			session['email'] = newuser.email			
+			return redirect(url_for('profile'))		
+			
+	elif request.method == 'GET':
+		return render_template('signup.html', form=form)
 		
 @app.route('/signout')
 def signout():
@@ -101,6 +106,13 @@ def signout():
 	
 if __name__ == '__main__':
 	app.run(debug=True)
+	
+@app.route('/testdb')
+def testdb():
+	if db.session.query("1").from_statement("SELECT 1").all():
+		return render_template('database.html', works=True)
+	else:
+		return render_template('database.html', works=False)
 
 
 
