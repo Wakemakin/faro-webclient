c411209768847880f52cf9acf34d14df8e4290c7
Dennis Jordan <dj1042@gmail.com>
2013-07-12 20:34:14 -0500

    Added test code to wrapper.

---
+++ faro/static/js/faro-api.wrapper.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+	
+	$(".GET").click(function(event) {
+		var url = "http://www.jibely.com:5001/api/users";
+		$.getJSON(url + "?callback=?", function(users){
+			console.log(users.total);
+		});
+	});
+	
+	$(".push2").click(function(event) {
+		alert("Push 2");
+		console.log("Push 2");
+	});
+	
+	$(".push3").click(function(event) {
+		alert("Push 3");	
+		console.log("Push 3");
+	});
+			
+});
