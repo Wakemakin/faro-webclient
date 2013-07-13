ec164e80efe9afcbf23148d8cd58c444a45232b4
Dennis Jordan <dj1042@gmail.com>
2013-07-12 23:13:23 -0500

    Updated poster app.

--- faro/static/js/tmdb-api.wrapper.js
+++ faro/static/js/tmdb-api.wrapper.js
@@ -1,23 +1,27 @@
 $(document).ready(function() {
 	
-	// removes no poster message
+	// removes messages
 	$("#term").focus(function() {
 		var full = $("#poster").has("img").length ? true : false;
 		if (full == false) {
-			$('#poster').empty();
+			$('#message').empty();
 		}
 	});
+	
+	var loadBackdrops = function(backdrops) {
+		
+	};
 	
 	// functions
 	var getPoster = function() {
 		// grab movie title and store in a variable
 		var film = $('#term').val();
 		if (film == '') {
-			$('#poster').html("<h3 class='loading'>Please enter something!</h3>");
+			$('#message').html("<h3 class='loading'>Please enter something!</h3>");
 		}
 		else {
 			// display loading message
-			$('#poster').html("<h3 class='loading'>Loading up poster!</h3>");
+			$('#message').html("<h3 class='loading'>Loading up poster!</h3>");
 			$.getJSON("http://api.themoviedb.org/2.1/Movie.search/en/json/23afca60ebf72f8d88cdcae2c4f31866/" + 
 					film + "?callback=?", function(json) { 
 				
@@ -26,12 +30,16 @@ $(document).ready(function() {
 				// check if poster was found
 				if (json[0] != "Nothing found.") {
 					// Display the poster and a mesasage announcing the result
-					$('#poster').html('<h3 class="loading">Found it!</h3><img id="thePoster" src=' + json[0].posters[4].image.url + ' />');
+					$('#message').html('<h3 class="loading">Found it!</h3>');
+					$('#poster').html('<img id="thePoster" src=' + json[0].posters[2].image.url + ' />');
+					loadBackdrops(json[0].backdrops);
 				}
 				else {
 					 $.getJSON("http://api.themoviedb.org/2.1/Movie.search/en/json/23afca60ebf72f8d88cdcae2c4f31866/goonies?callback=?", 
 							 function(json) {
-						 $('#poster').html('<h3 class="loading">Found nothing... Maybe you were looking for The Goonies?</h3><img id="thePoster" src=' + json[0].posters[4].image.url + ' />');
+						 $('#message').html('<h3 class="loading">Found nothing... Maybe you were looking for The Goonies?</h3>');
+						 $('#poster').html('<img id="thePoster" src=' + json[0].posters[2].image.url + ' />');
+						 loadBackdrops(json[0].backdrops);
 					 });
 				}
 			});
