9da126b43963a15df64298b3c5d8c2e98ccd76df
Dennis Jordan <dj1042@gmail.com>
2013-07-29 20:35:11 -0500

    Updated model

--- faro/static/js/faro-api.js
+++ faro/static/js/faro-api.js
@@ -4,8 +4,8 @@
  * FaroModel provides a structure to save, update, load and remove 
  * model data. This class is meant to be extended.  The subclass
  * needs to set the url {string} property to the resource it plans to 
- * use. In addition, there are several success and error functions 
- * that need to be overridden for the save, load and remove methods.
+ * use/modify. In addition, there are several success and error functions 
+ * that need to be overridden for the save, update, load and remove methods.
  * 
  * @constructor
  */
@@ -20,7 +20,16 @@ function FaroModel() {
 	self.url = '';
 	
 	/**
-	 * Saves the model data.
+	 * The primary key of the data model in
+	 * the database.
+	 * 
+	 * @define {string}
+	 */
+	self.key = ''
+	
+	/**
+	 * Saves the model into the database with the
+	 * specified json string.
 	 * 
 	 * @param {string} json: json string
 	 */
@@ -36,77 +45,102 @@ function FaroModel() {
 	};
 	
 	/**
-	 * Loads the model data.
+	 * Updates the model in the database with the
+	 * specified json string.
 	 * 
-	 * @param {string} uuid: uuid of model
+	 * @param {string} json: json string
 	 */
-	self.load = function(uuid) {
+	self.update = function(json) {
 		$.ajax({
-			type: 'GET',
-			url: self.url + '/' + uuid,
-			contentType: 'text/plain',
-			dataType: 'json',
-			success: self.loadSuccess(data, textStatus, request),
-			error: self.loadError(request, textStatus)
+			type: 'PUT',
+			url: self.url + '/' + self.key,
+			data: json,
+			contentType: 'text/plain', 
+			success: self.updateSuccess(data, textStatus, request),
+		    error: self.updateError(request, textStatus)
 		});
 	};
 	
 	/**
-	 * Removes the model data.
+	 * Loads the model from the database with the 
+	 * specified id.
 	 * 
-	 * @param {string} uuid: uuid of model
+	 * @param {string} id: id of data model in database
 	 */
-	self.remove = function(uuid) {
+	self.load = function(id) {
+		$.ajax({
+			type: 'GET',
+			url: self.url + '/' + id,
+			contentType: 'text/plain',
+			dataType: 'json',
+			success: self.loadSuccess,
+			error: self.loadError
+		});
+	};
+	
+	/**
+	 * Removes the model from the database.
+	 */
+	self.remove = function() {
 		$.ajax({
 			type: 'DELETE',
-			url: self.url + '/' + uuid,
+			url: self.url + '/' + key,
 			success: self.removeSucess(data, textStatus, request),
 			error: self.removeError(request, textStatus)
 		});
 	};
 	
 	/**
+	 * Override to provide an action on save success.
 	 * 
-	 * @param {object} data:
-	 * @param {string} textStatus:
-	 * @param {object} request:
+	 * @param {object}       data: data returned from server
+	 * @param {string} textStatus: status of request
+	 * @param {object}    request: jqXHR object
 	 */
 	self.saveSuccess = function(data, textStatus, request) { };
 	
 	/**
-	 * 
-	 * @param {object} request:
-	 * @param {string} textStatus:
+	 * Override to provide an action on save error.
+	 *  
+	 * @param {object}       jqXHR: jqXHR object
+	 * @param {string}  textStatus: error status
+	 * @param {string} errorThrown: optional exception object
 	 */
-	self.saveError = function(request, textStatus) { };
+	self.saveError = function(request, textStatus, errorThrown) { };
 	
 	/**
-	 * 
-	 * @param {object} data:
-	 * @param {string} textStatus:
-	 * @param {object} request:
+	 * Override to provide an action on load success.
+	 *  
+	 * @param {object}       data: data returned from server
+	 * @param {string} textStatus: status of request
+	 * @param {object}    request: jqXHR object
 	 */
 	self.loadSuccess = function(data, textStatus, request) { };
 	
 	/**
-	 * 
-	 * @param {object} request:
-	 * @param {string} textStatus:
+	 * Override to provide an action on load error.
+	 *  
+	 * @param {object}       jqXHR: jqXHR object
+	 * @param {string}  textStatus: error status
+	 * @param {string} errorThrown: optional exception object
 	 */
-	self.loadError = function(request, textStatus) { };
+	self.loadError = function(request, textStatus, errorThrown) { };
 	
 	/**
-	 * 
-	 * @param {object} data:
-	 * @param {string} textStatus:
-	 * @param {object} request:
+	 * Override to provide an action on remove success.
+	 *  
+	 * @param {object}       data: data returned from server
+	 * @param {string} textStatus: status of request
+	 * @param {object}    request: jqXHR object
 	 */
 	self.removeSuccess = function(data, textStatus, request) { };
 	
 	/**
+	 * Override to provide an action on remove error.
 	 * 
-	 * @param {object} request:
-	 * @param {string} textStatus:
+	 * @param {object}       jqXHR: jqXHR object
+	 * @param {string}  textStatus: error status
+	 * @param {string} errorThrown: optional exception object
 	 */
-	self.removeError = function(request, textStatus) { };
+	self.removeError = function(request, textStatus, errorThrown) { };
 }
