60d0c63579fb7e1668f570480b6d46ad58aa5b3c
Dennis Jordan <dj1042@gmail.com>
2013-08-03 13:33:06 -0500

    Added logger for jenkins integration

--- faro/static/js/test/test.js
+++ faro/static/js/test/test.js
@@ -1,3 +1,14 @@
-test( "hello test", function() {
+/**
+ * User model tests.
+ */
+test( "User()", function() {
+	var user = new User();
   ok( 1 == "1", "Passed!" );
+  ok( 2 == "2", "Passed!" );
+  ok( 3 == "3", "Passed!" );
 });
+
+/*
+QUnit.jUnitReport = function(report) {
+	console.log(report.xml);
+} */
